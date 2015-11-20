c93b3b1d6bbdb247ec1c99ac4f7b195ed06e1555
Anthony FLORES <atmospherick@gmail.com>
2015-11-20 06:34:17 +0100

    Update cachematrix.R
    adding comments for the second test

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,25 @@
 ## Write a short comment describing this function
 
 ## test fonction donnée par énoncé
+## JUST an empty function test for myself for the github side
 makeVector <- function(x = numeric()) {
         m <- NULL
+        
+        ## first function created inside the makeVector function - SET function
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        
+        ## second function created inside the makeVector function - GET function
         get <- function() x
+        
+        ## third function created inside the makeVector function - SETMEAN function
         setmean <- function(mean) m <<- mean
+        
+        ## fourth function created inside the makeVector function - GETMEAN function
         getmean <- function() m
+        
         list(set = set, get = get,
              setmean = setmean,
              getmean = getmean)

0e1fb3dccc2893f9b58e6e1c799f4d51b0ea3649
Anthony FLORES <atmospherick@gmail.com>
2015-11-20 06:25:02 +0100

    Update cachematrix.R
    adding function given on instructions just for a FIRST test for github

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,24 @@
 
 ## Write a short comment describing this function
 
+## test fonction donnée par énoncé
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+## fin test fonction donnée par énoncé
+##################################################################
+
 makeCacheMatrix <- function(x = matrix()) {
 
 }
